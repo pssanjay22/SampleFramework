36bbb0a21c041ae23a91222d9e051e90d568555f
naveen magnur <naveen magnur@magnur>
2022-07-18 15:21:43 +0530

    This framework is for practice

---
+++ src/test/java/sampleframework/CreateContactTest.java
@@ -0,0 +1,16 @@
+package sampleframework;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void createContact() {
+		System.out.println("==============contact created=============");
+	}
+	
+	@Test
+	public void createProduct() {
+		System.out.println("==========product created============");
+	}
+}
