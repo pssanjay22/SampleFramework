f4782181aeca0c1ac74dd14f11f20388878f0436
sheno <sheno@HVSL>
2022-07-19 16:26:19 +0530

    This is for jenkins

--- src/test/java/sampleframework/CreateContactTest.java
+++ src/test/java/sampleframework/CreateContactTest.java
@@ -6,16 +6,16 @@ public class CreateContactTest {
 
 	@Test
 	public void createContact() {
-		System.out.println("==============contact created=============");
+		System.out.println("==============contact created=============.");
 	}
 	
 	@Test
 	public void createProduct() {
-		System.out.println("<==========product created============>");
+		System.out.println("<==========product created============>.");
 	}
 	
 	public void createCampaign()
 	{
-		System.out.println("==========Campaign created============");
+		System.out.println("<==========Campaign created============>");
 	}
 }
